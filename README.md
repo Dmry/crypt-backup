a49a3e001cedbb6360be8b8dbd4021f6b80948b6
sound <sound@dmry.nl>
2018-04-08 16:00:52 +0200

    Everything works, version 1.0.

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ The script will mount the encrypted disk created above.
  Next, it creates a folder named backup in your /media folder,
  so please check if there are any conflicts before running.
  Finally it creates a disk image named FILENAME.img of INTERNAL on EXTERNAL.
+ Progress can be monitored with `sudo cat /media/backup/progress.log`.
  When the backup is finished, the LUKS container will be dismounted and closed automatically.
 
 ### Set permissions and run script
--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-EXTERNAL=<fill in external disk locations (e.g. /dev/sdb) here>;
-INTERNAL=<fill in internal disk locations (e.g. /dev/sda1) here>;
-FILENAME=<fill in filename.img of backup here (e.g. computer.img)>;
+EXTERNAL=/dev/sdb;
+INTERNAL=/dev/sda1;
+FILENAME=server.img;
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root."
@@ -17,13 +17,13 @@ sudo fdisk -l | grep $INTERNAL
 read -r -p "Is this correct [Y/n]" RESPONSE
 case "$RESPONSE" in
         [yY][eE][sS]|[yY])
-                mkdir /media/backup &>dev/null
+                mkdir /media/backup >dev/null 2>dev/null
                 cryptsetup luksOpen $EXTERNAL backup
                 mount /dev/mapper/backup /media/backup
 
 		cd /media/backup
 		echo "Creating backup, this can take a while.."
-                dd if=$INTERNAL of=/media/backup/$FILENAME status=progress > progress.log
+                dd if=$INTERNAL of=/media/backup/$FILENAME status=progress &>progress.log
 		umount /media/backup
 		cryptsetup close backup
                 ;;
