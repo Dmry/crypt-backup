188633a4c8afb671e91f9bcdc96a5b2697026f2a
dmry <dmry.nl>
2018-04-08 14:09:09 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Preparation / installation
+
+### Install dependency
+
+sudo apt-get install cryptsetup
+
+### Create encrypted partition on destination drive
+``sudo cryptsetup -v --verify-passphrase luksFormat $EXTERNAL
+sudo cryptsetup luksOpen $EXTERNAL backup
+sudo mkfs.ext4 /dev/mapper/backup
+sudo cryptsetup luksClose /dev/mapper/backup``
+
+### Set permissions and run script
+cd into the directory containing backup.sh
+
+When running for the first time:
+`sudo chmod u+x backup.sh`
+
+Then run
+`./backup.sh`
+
+### Kill when daemonized:
+ps -ef | grep "command name"
+
+# Restore backup
+Find the location of the drive containing your backup and a possible destination disk.
+`sudo fdisk -l`
+
+Open and mount this location. Replace /path/to/drive/with backup with the correct path found with fdisk.
+``cryptsetup luksOpen /path/to/drive/with/backup backup
+mount /dev/mapper/backup /media/backup``
+
+Replace /path/to/drive/of/destination with the correct path found with fdisk.
+`sudo dd if=media/backup/server.img of=/path/to/drive/of/destination status=progress`
---
+++ backup.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+EXTERNAL=<fill in external disk locations (e.g. /dev/sdb) here>;
+INTERNAL=<fill in internal disk locations (e.g. /dev/sda1) here>;
+FILENAME=<fill in filename.img of backup here (e.g. computer.img)>;
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
+echo "Listing specified disks..."
+echo "Destination:"
+sudo fdisk -l | grep $EXTERNAL
+echo "Original:"
+sudo fdisk -l | grep $INTERNAL
+read -r -p "Is this correct [Y/n]" RESPONSE
+case "$RESPONSE" in
+        [yY][eE][sS]|[yY])
+                eclipse &>mkdir /media/backup
+                cryptsetup luksOpen $EXTERNAL backup
+                mount /dev/mapper/backup /media/backup
+
+                read -r -p "Daemonize process? (Recommended for SSH sessions)" DAEMONIZE
+                case $DAEMONIZE in
+                        [yY][eE][sS]|[yY])
+				cd /media/backup
+                                nohup dd if=$INTERNAL of=/media/backup/$FILENAME;;
+                        *)
+                        dd if=$INTERNAL of=/media/backup/$FILENAME status=progress
+                        ;;
+                esac
+                ;;
+        *)
+                #or exit..
+                echo "Please adjust disk locations in script file."
+                exit
+                ;;
+esac
+
